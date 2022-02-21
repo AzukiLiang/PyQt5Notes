d19e3bf658ad686be40128561c679023e27e5f30
liang <601038667@qq.com>
2022-02-21 15:14:09 +0800

    Default Changelist

--- pythonTest/python_test.py
+++
@@ -1,4 +0,0 @@
-import hello
-
-name = 'Li Ming'
-hello.say_hello_to(name)
--- pythonTest/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    name='Hello_world_app',
-    ext_modules=cythonize("hello.pyx"),
-    zip_safe=False,)
